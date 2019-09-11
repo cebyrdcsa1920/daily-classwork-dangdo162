ad741dc1474530732100084b868be8c3a31ba8a8
dangdo162 <dangdo162@yahoo.com>
2019-09-11 13:58:52 +0000

    create method to add gas to car

--- week4/Car.java
+++ week4/Car.java
@@ -1,12 +1,28 @@
 import java.awt.Color;
+
 public class Car
 {
     private String model;
     private Color color;
     private int numPassengers;
     private double amountOfGas;
-    mpg = 15.0;
+    private final double mpg;
 }
+
+    public Car()
+    {
+        model = "Mustang";
+        color = Color.YELLOW;
+        numPassengers = 2;
+        amountOfGas = 12.5;
+        mpg = 21;
+    }
+    /**
+     * Drives the car the given distance. Subtracts gas from the tank.
+     *
+     * @param distance the distance driven in miles
+     */
+
 
 public void drive(double distance)
     {
@@ -34,14 +50,6 @@ public void drive(double distance)
          amountOfGas += amount;
      }
 
-
-    public Car()
-    {
-        model = "Mustang";
-        color = Color.YELLOW;
-        numPassengers = 2;
-        amountOfGas = 12.5;
-    }
 
 
     public String toString()
--- week4/CarRunner.java
+++ week4/CarRunner.java
@@ -1,10 +1,13 @@
-import java.awt.Color;
 public class CarRunner
 {
     public static void main(String[] args)
     {
         Car myMustang = new Car();
-        System.out.println(myMustang);
+        //System.out.println(myMustang);
+        myMustang.drive(5.5);
+        System.out.println(myMustang.getAmountOfGas());
+        myMustang.drive(100.5);
+        System.out.println(myMustang.getAmountOfGas());
     }
 
 }
